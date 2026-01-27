4877992067f7ee6865187c0ef5cc5c0448151954
Ryan Fahey <git@ryan.science>
2026-01-26 19:12:49 -0500

    changes

--- app/(kambaz)/courses/[cid]/modules/page.tsx
+++ app/(kambaz)/courses/[cid]/modules/page.tsx
@@ -10,13 +10,24 @@ export default function Modules() {
 
             <ul id="wd-modules">
                 <li className="wd-module">
-                    <div className="wd-title">Week 1</div>
+                    <div className="wd-title">Week 1 - Monday</div>
                     <ul className="wd-lessons">
                         <li className="wd-lesson">
                             <span className="wd-title">LEARNING OBJECTIVES</span>
                             <ul className="wd-content">
                                 <li className="wd-content-item">Introduction to the course</li>
                                 <li className="wd-content-item">Learn what is Web Development</li>
+                            </ul>
+                        </li>
+                    </ul>
+
+                    <div className="wd-title">Week 1 - Thursday</div>
+                    <ul className="wd-lessons">
+                        <li className="wd-lesson">
+                            <span className="wd-title">LEARNING OBJECTIVES</span>
+                            <ul className="wd-content">
+                                <li className="wd-content-item">Learn HTML basics</li>
+                                <li className="wd-content-item">Learn Vercel Deployment</li>
                             </ul>
                         </li>
                     </ul>
