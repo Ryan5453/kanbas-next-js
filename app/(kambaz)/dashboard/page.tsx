709aa13316c32fea217d00773b0e4d0677d0dd5d
Ryan Fahey <git@ryan.science>
2026-02-16 20:42:38 -0500

    everything besides the do it on your own

--- app/(kambaz)/dashboard/page.tsx
+++ app/(kambaz)/dashboard/page.tsx
@@ -1,140 +1,32 @@
 import Link from "next/link";
+import * as db from "../database";
 import { Row, Col, Card, CardImg, CardBody, CardTitle, CardText, Button } from "react-bootstrap";
 
 export default function Dashboard() {
+  const courses = db.courses;
   return (
     <div id="wd-dashboard">
-      <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
-      <h2 id="wd-dashboard-published">Published Courses (7)</h2> <hr />
-      <div id="wd-dashboard-courses">
-        <Row xs={1} md={5} className="g-4">
-          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-            <Card>
-              <Link href="/courses/1234/home"
-                className="wd-dashboard-course-link text-decoration-none text-dark">
-                <CardImg variant="top" src="/images/research.avif" width="100%" height={160} />
-                <CardBody>
-                  <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">
-                    CS4545 Research Practicum
-                  </CardTitle>
-                  <CardText className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
-                    Conduct original research with faculty mentorship in a specialized area of computer science.
-                  </CardText>
-                  <Button variant="primary">Go</Button>
-                </CardBody>
-              </Link>
-            </Card>
-          </Col>
-
-          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-            <Card>
-              <Link href="/courses/1234/home"
-                className="wd-dashboard-course-link text-decoration-none text-dark">
-                <CardImg variant="top" src="/images/ai.avif" width="100%" height={160} />
-                <CardBody>
-                  <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">
-                    CS4100 Artificial Intelligence
-                  </CardTitle>
-                  <CardText className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
-                    Explore foundational AI concepts including search, logic, and machine learning.
-                  </CardText>
-                  <Button variant="primary">Go</Button>
-                </CardBody>
-              </Link>
-            </Card>
-          </Col>
-
-          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-            <Card>
-              <Link href="/courses/1234/home"
-                className="wd-dashboard-course-link text-decoration-none text-dark">
-                <CardImg variant="top" src="/images/ai-security.jpg" width="100%" height={160} />
-                <CardBody>
-                  <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">
-                    CY4100 AI Security and Privacy
-                  </CardTitle>
-                  <CardText className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
-                    Learn about security vulnerabilities and privacy concerns in AI systems.
-                  </CardText>
-                  <Button variant="primary">Go</Button>
-                </CardBody>
-              </Link>
-            </Card>
-          </Col>
-
-          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-            <Card>
-              <Link href="/courses/1234/home"
-                className="wd-dashboard-course-link text-decoration-none text-dark">
-                <CardImg variant="top" src="/images/react.webp" width="100%" height={160} />
-                <CardBody>
-                  <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">
-                    CS4550 Web Development
-                  </CardTitle>
-                  <CardText className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
-                    Full Stack software development using React, Node.js, and MongoDB.
-                  </CardText>
-                  <Button variant="primary">Go</Button>
-                </CardBody>
-              </Link>
-            </Card>
-          </Col>
-
-          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-            <Card>
-              <Link href="/courses/1234/home"
-                className="wd-dashboard-course-link text-decoration-none text-dark">
-                <CardImg variant="top" src="/images/algo.png" width="100%" height={160} />
-                <CardBody>
-                  <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">
-                    CS3000 Algorithms and Data
-                  </CardTitle>
-                  <CardText className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
-                    Study algorithm design, analysis, and fundamental data structures.
-                  </CardText>
-                  <Button variant="primary">Go</Button>
-                </CardBody>
-              </Link>
-            </Card>
-          </Col>
-
-          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-            <Card>
-              <Link href="/courses/1234/home"
-                className="wd-dashboard-course-link text-decoration-none text-dark">
-                <CardImg variant="top" src="/images/ml.jpg" width="100%" height={160} />
-                <CardBody>
-                  <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">
-                    DS4400 Machine Learning
-                  </CardTitle>
-                  <CardText className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
-                    Machine Learning and Data Mining techniques for real-world applications.
-                  </CardText>
-                  <Button variant="primary">Go</Button>
-                </CardBody>
-              </Link>
-            </Card>
-          </Col>
-
-          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-            <Card>
-              <Link href="/courses/1234/home"
-                className="wd-dashboard-course-link text-decoration-none text-dark">
-                <CardImg variant="top" src="/images/toc.jpeg" width="100%" height={160} />
-                <CardBody>
-                  <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">
-                    CS3800 Theory of Computation
-                  </CardTitle>
-                  <CardText className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
-                    Explore automata theory, formal languages, and computational complexity.
-                  </CardText>
-                  <Button variant="primary">Go</Button>
-                </CardBody>
-              </Link>
-            </Card>
-          </Col>
-        </Row>
-      </div>
+     <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
+     <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2> <hr />
+     <div id="wd-dashboard-courses">
+      <Row xs={1} md={5} className="g-4">
+       {courses.map((course) => (
+       <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+        <Card>
+         <Link href={`/courses/${course._id}/home`}
+          className="wd-dashboard-course-link text-decoration-none text-dark" >
+          <CardImg src="/images/reactjs.webp" variant="top" width="100%" height={160} />
+          <CardBody className="card-body">
+           <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">
+            {course.name} </CardTitle>
+           <CardText className="wd-dashboard-course-description overflow-hidden" style={{height:"100px"}}>
+            {course.description} </CardText>
+           <Button variant="primary"> Go </Button>
+          </CardBody>
+         </Link>
+        </Card>
+       </Col>
+      ))}
+     </Row>
     </div>
-  );
-}
+   </div>);}
